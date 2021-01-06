c8224dcee4753456e6e091d4246e0e8825a6f03d
Maxim Shakurov <m.shakurov99@mail.ru>
2021-01-06 23:19:40 +0300

    Добавьте файлы проекта.

---
+++ ZI_SystemWatcher/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace ZI_SystemWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddFound = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Founder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Отслеживается = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddFound
+            // 
+            this.AddFound.Location = new System.Drawing.Point(12, 446);
+            this.AddFound.Name = "AddFound";
+            this.AddFound.Size = new System.Drawing.Size(126, 23);
+            this.AddFound.TabIndex = 0;
+            this.AddFound.Text = "Добавить папку";
+            this.AddFound.UseVisualStyleBackColor = true;
+            this.AddFound.Click += new System.EventHandler(this.AddFound_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Founder,
+            this.Отслеживается});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(464, 342);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.CurrentCellDirtyStateChanged += new System.EventHandler(this.dataGridView1_CurrentCellDirtyStateChanged);
+            // 
+            // Founder
+            // 
+            this.Founder.HeaderText = "Папка";
+            this.Founder.Name = "Founder";
+            this.Founder.ReadOnly = true;
+            this.Founder.Width = 300;
+            // 
+            // Отслеживается
+            // 
+            this.Отслеживается.HeaderText = "Check";
+            this.Отслеживается.Name = "Отслеживается";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Путь к папке";
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(15, 416);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(156, 13);
+            this.labelError.TabIndex = 5;
+            this.labelError.Text = "Путь к папке указан неверно";
+            this.labelError.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(483, 13);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(305, 341);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 481);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.AddFound);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddFound;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Founder;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Отслеживается;
+    }
+}
+
---
+++ ZI_SystemWatcher/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Permissions;
+using System.Data.SqlClient;
+
+namespace ZI_SystemWatcher
+{
+    public partial class Form1 : Form
+    {
+        List<FileSystemWatcher> Wachers = new List<FileSystemWatcher>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddFound_Click(object sender, EventArgs e)
+        {
+            labelError.Visible = false;
+            try 
+            {
+                Directory.EnumerateFiles(textBox1.Text);
+                dataGridView1.Rows.Add(textBox1.Text, true);
+                FileSystemWatcher FSW = new FileSystemWatcher();
+                FSW.Path = textBox1.Text;
+
+                FSW.Changed += fileSystemWatcher1_Changed;
+                FSW.Deleted += fileSystemWatcher1_Deleted;
+                FSW.Renamed += fileSystemWatcher1_Renamed;
+                FSW.Created += fileSystemWatcher1_Created;
+
+                FSW.IncludeSubdirectories = true;
+                FSW.EnableRaisingEvents = true;
+                Wachers.Add(FSW);
+
+                SqlCommand command = new SqlCommand("INSERT INTO [Folders](Path)VALUES(@Path)", Connect);
+                command.Parameters.AddWithValue("Path", textBox1.Text);
+
+                textBox1.Clear();
+                
+                
+            }
+            catch 
+            {
+                labelError.Visible = true;
+            }
+            
+
+        }
+
+
+
+      
+
+        private void fileSystemWatcher1_Deleted(object sender, FileSystemEventArgs e)
+        {
+            textBox2.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += e.FullPath + " удален" + "\r\n";
+            });   
+        }
+
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            textBox2.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += e.FullPath + " cоздан" + "\r\n";
+            });
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            textBox2.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += e.FullPath + " изменен" + "\r\n";
+            });
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, RenamedEventArgs e)
+        {
+            textBox2.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += e.FullPath + " переименован" + "\r\n";
+            });
+        }
+
+        private void dataGridView1_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.IsCurrentCellDirty) 
+            {
+                dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);   
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (Wachers.Count > 0)
+            {
+                Wachers[e.RowIndex].EnableRaisingEvents = !Wachers[e.RowIndex].EnableRaisingEvents;
+            }
+        }
+    }
+}
+
